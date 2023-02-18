532ae18b64b7c9d422e4ca169b783533198dad49
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-18 12:18:07 +0500

    find user massege count

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -11,4 +11,19 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    return
+    massege_user: dict= {}
+   
+    for userId in user_id:
+        # print(userId)
+        massage_count: int = 0
+        for user in data["messages"]:
+            if user["type"] == "message" and user["from_id"] == userId:
+                massage_count += 1
+        massege_user.update({userId: massage_count})
+    return massege_user
+                
+
+    
+data = read_data('data/result.json')
+user_id = find_all_users_id(data)
+# print(find_user_message_count(data, user_id))
